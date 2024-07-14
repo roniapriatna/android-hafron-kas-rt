36deed4ae5a7295d97e33ea4a45fba9b79a5c1f7
Hafsyah182 <hafsyahsyabrina@gmail.com>
2024-07-14 09:31:46 +0700

    fitur: Add readme

--- app/src/main/java/com/example/hafronkasrt/view/fagment/pemasukan/PemasukanViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.hafronkasrt.view.fagment.pemasukan;
-
-import androidx.lifecycle.ViewModel;
-
-public class PemasukanViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
--- app/src/main/java/com/example/hafronkasrt/view/fagment/pengeluaran/PengeluaranViewModel.java
+++
@@ -1,7 +0,0 @@
-package com.example.hafronkasrt.view.fagment.pengeluaran;
-
-import androidx.lifecycle.ViewModel;
-
-public class PengeluaranViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
-}
